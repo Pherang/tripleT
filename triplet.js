96bc5f4104cde4ce0d21975546f0c570b6d3e9ee
pherang <pherang@users.noreply.github.com>
2018-03-04 17:00:05 -0500

    Created layout for tic tac toe board

--- triplet.js
+++ triplet.js
@@ -6,6 +6,8 @@
 * 
 */
 
+
+
 /*
 * Valid wins
 * 123
@@ -20,47 +22,60 @@
 
 /* 
 * Computer's logic to win:
-*
+* 123
+* 456
+* 789
 * First turn
 * user picks 5, computer picks 2,4,6,8.
 * user picks anything else, computer picks 5.
 *
+* 123
+* OX6
+* 789
 *
+* User picks another spot on the grid.
+* Computer checks to see if that spot leads to a win.
+* 
+* 1X3
+* OX6
+* 789
 *
-*
+* Computer should check to see if the user has selected two numbers in any of the valid wins.
+* If the user has then the computer selects the third number that the user needs to win.
+* If the user hasn't selected any two in a row then the computer checks to see if it has any two in a row.
+* If it does then it tries to select the third number to win.
+* If the computer doesn't have two in a row then it selects a number to build toward three in a row.
+* 
 */
 
+function computerTurn () {
+
+}
 /* 
 * Need some way of translating or tracking the positions 
 * [0][0] [1][0] [2][0]
 * [0][1] [1][1] [2][1]
 * [0][2] [1][2] [2][2]
 */
-var gameGrid = [[1,2,3],[4,5,6],[7,8,9]];
+var gameGrid = [[1,4,7],[2,5,8],[3,6,9]];
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+/*
+* Need to allow user to pick X or O. If user picks one the computer is the other.
+*/
 
 
 /*
 * Need a way to switch and detect turns
 */
 
+
 /*
 * Need a way to detect board is full
 */
+
 
 /*
 * Need a way to detect a win
