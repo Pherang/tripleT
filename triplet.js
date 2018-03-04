4102f68ff80b8f79038848f92ce4d8914230f6fd
pherang <pherang@users.noreply.github.com>
2018-03-04 06:27:35 -0500

    Initialize files for project

---
+++ triplet.js
@@ -0,0 +1,35 @@
+/*
+* Need to figure out how to track tac tic toe 
+* It is a 3x3 grid.
+* Computer should block and two X's in a row.
+* Computer should try to get its own 3 in a row of course.
+* 
+*/
+
+/*
+Valid wins
+
+Grid
+123
+456   
+789   
+
+123
+
+456
+
+789
+
+147
+258
+369
+
+159
+356
+
+user picks 5, computer picks 2,4,6,8.
+user picks anything else, computer picks 5.
+
+
+
+*/
