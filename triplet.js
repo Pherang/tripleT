11fd590f4d04ced9cb325b3ab6b93b51a3d456f5
unknown <Pherang@users.noreply.github.com>
2018-03-04 07:53:42 -0500

    More comments to workout code

--- triplet.js
+++ triplet.js
@@ -1,35 +1,67 @@
 /*
 * Need to figure out how to track tac tic toe 
 * It is a 3x3 grid.
-* Computer should block and two X's in a row.
-* Computer should try to get its own 3 in a row of course.
+* Computer should block two X's in a row.
+* Computer should try to get its own three in a row of course.
 * 
 */
 
 /*
-Valid wins
+* Valid wins
+* 123
+* 456
+* 789
+* 147
+* 258
+* 369
+* 159
+* 356
+*/
 
-Grid
-123
-456   
-789   
+/* 
+* Computer's logic to win:
+*
+* First turn
+* user picks 5, computer picks 2,4,6,8.
+* user picks anything else, computer picks 5.
+*
+*
+*
+*
+*/
 
-123
-
-456
-
-789
-
-147
-258
-369
-
-159
-356
-
-user picks 5, computer picks 2,4,6,8.
-user picks anything else, computer picks 5.
-
+/* 
+* Need some way of translating or tracking the positions 
+* [0][0] [1][0] [2][0]
+* [0][1] [1][1] [2][1]
+* [0][2] [1][2] [2][2]
+*/
+var gameGrid = [[1,2,3],[4,5,6],[7,8,9]];
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+* Need a way to switch and detect turns
+*/
+
+/*
+* Need a way to detect board is full
+*/
+
+/*
+* Need a way to detect a win
 */
