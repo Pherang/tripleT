6fc0cafd06c4eda397e1ed2b014c2441cd961c44
unknown <Pherang@users.noreply.github.com>
2018-03-06 07:58:19 -0500

    Continuing work on computer logic

--- triplet.js
+++ triplet.js
@@ -6,8 +6,6 @@
 * 
 */
 
-
-
 /* 
 * Computer's logic to win:
 * 123
@@ -28,27 +26,33 @@
 * OX6
 * 789
 *
-* Computer should check to see if the user has selected two numbers in any of the valid wins.
+* Computer should check to see if the user has selected two numbers in any of the valid wins for the X they just marked.
 * If the user has then the computer selects the third number that the user needs to win.
 * If the user hasn't selected any two in a row then the computer checks to see if it has any two in a row.
 * If it does then it tries to select the third number to win.
 * If the computer doesn't have two in a row then it selects a number to build toward three in a row.
 * 
 */
-
+function checkRowOne () {
+  
+  if (gameGrid[0][0] == playerMark) {}
+  
+}
 function computerTurn (playerspick) {
-  console.log("wheeee");
-  console.log(playerspick);
+  console.log('wheeee');
+  console.log('Player marked ' + playerspick);
+  console.log(gameGrid);
   switch (playerspick) {
 
     case 'one':
-      console.log("case detected");
-      if (gameGrid['two'] === '') {
-        console.log("two will be marked");
-        gameGrid['two'] = computerMark;
-        setTimeout( markSpot(computerMark,two), 5000);
-      }
-
+    
+      break;
+    case 'two':
+   
+      break;
+      case 'three':
+   
+      break;
   }
 
 }
@@ -62,11 +66,26 @@ function computerTurn (playerspick) {
 * 
 * An array might not be the best for this actually.
 * It might be better as an object..
+* No, an array is better
+*
+* What if it was eight arrays?
+* [one,two,three]
+* [four,five,six]
+* [seven,eight,nine]
+*
+* [one,four,seven]
+* [two,five,eight]
+* [three,six,nine]
+*
+* [one,five,nine]
+* [three,five,six]
+*
 *
 */
-// var gameGrid = [[1,4,7],[2,5,8],[3,6,9]];
+var gameGrid = [[1,4,7],[2,5,8],[3,6,9]];
+console.log('Grid at 0 0 is ' + gameGrid[0][0]);
 
-var gameGrid = {
+/*var gameGrid = {
 
   one : '',
   two : '',
@@ -78,23 +97,19 @@ var gameGrid = {
   eight : '',
   nine : '',
   
-}
-
+}*/
 
 /*
 * Need to allow user to pick X or O. If user picks one the computer is the other.
 */
 
-
 /*
 * Need a way to switch and detect turns
 */
 
-
 /*
 * Need a way to detect board is full
 */
-
 
 /*
 * Need a way to detect a win
@@ -108,9 +123,33 @@ function detectWin(boardObject){
   }
 
 }
+/*
+* Need function to translate visual grid spots to locations in array gameGrid
+*/
 
+function translateGrid (spot) {
+  console.log("Called me");
+  console.log('Spot is ' + spot);
+  switch (spot) {
+    case one:
+      console.log(spot);
+      return gameGrid[0][0];
+      break;
+    case two:
+      return gameGrid[0][1];
+      break;
+    case three:
+      return gameGrid[0][2];
+      break;  
+    case 'four':
+      console.log('four be returned');
+      console.log(gameGrid[0][1]);
+      return gameGrid[0][1];
+      break;
+          
+  }
 
-
+}
 
 /*
 * Need a  way to mark the board.
@@ -118,14 +157,16 @@ function detectWin(boardObject){
 playerMark = 'X';
 computerMark = 'O';
 
-function markSpot (XorO,gridSpot) {
+function markSpot (XorO, gridSpot) {
   gridSpot.textContent = XorO;
 }
 
-function playerTurn (playersMark,gridSpot) {
-  if (gameGrid[gridSpot.id] === '') {
-    gameGrid[gridSpot.id] = playersMark;
-    markSpot(playersMark,gridSpot);
+function playerTurn (playersMark, gridSpot) {
+  var gameSpot = translateGrid(gridSpot.id);
+  console.log(gameSpot);
+  if (gameSpot === '') {
+    gameSpot = playersMark;
+    markSpot(playersMark, gridSpot);
     detectWin(gameGrid);
     computerTurn(gridSpot.id);
   }
@@ -154,3 +195,53 @@ seven.addEventListener('click', function () { playerTurn(playerMark,this); }, fa
 eight.addEventListener('click', function () { playerTurn(playerMark,this); }, false );
 nine.addEventListener('click', function () { playerTurn(playerMark,this); }, false );
 
+/* Just in case I need this
+function computerTurn (playerspick) {
+  console.log('wheeee');
+  console.log('Player marked ' + playerspick);
+  console.log(gameGrid);
+  switch (playerspick) {
+
+    case 'one':
+      console.log('case detected');
+      if (gameGrid['two'] === '') {
+        
+        gameGrid['two'] = computerMark;
+        console.log('Grid is ' + gameGrid['two']);
+        setTimeout( markSpot(computerMark,two), 5000);
+      } else if (gameGrid['five'] === '') {
+        gameGrid['five'] = computerMark;
+        setTimeout( markSpot(computerMark,five), 5000);
+      } else if (gameGrid['four'] === '') {
+        gameGrid['four'] = computerMark;
+        setTimeout( markSpot(computerMark,four), 5000);
+      }
+      break;
+    case 'two':
+      console.log('case detected');
+      if (gameGrid['one'] === '') {
+        gameGrid['one'] = computerMark;
+        setTimeout( markSpot(computerMark,two), 5000);
+      } else if (gameGrid['three'] === '') {
+        gameGrid['three'] = computerMark;
+        setTimeout( markSpot(computerMark,five), 5000);
+      } else if (gameGrid['six'] === '') {
+        gameGrid['six'] = computerMark;
+        setTimeout( markSpot(computerMark,four), 5000);
+      }
+      break;
+      case 'three':
+      console.log('check whole row');
+      if (gameGrid['six'] === '') {
+        gameGrid['six'] = computerMark;
+        setTimeout( markSpot(computerMark,six), 5000);
+      } else if (gameGrid['nine'] === '') {
+        gameGrid['nine'] = computerMark;
+        setTimeout( markSpot(computerMark,nine), 5000);
+      } else if (gameGrid['two'] === '') {
+        gameGrid['two'] = computerMark;
+        setTimeout( markSpot(computerMark,two), 5000);
+      }
+      break;
+  }
+}*/
